8d9caa6504a8215fbbaea69e3790bfc15614b66e
Dale Kocian <dale.kocian@gmail.com>
2015-11-07 20:15:24 -0600

    feat: use palette api to set background color of place name holder

--- app/src/main/java/bhouse/travellist_starterproject/TravelListAdapter.java
+++ app/src/main/java/bhouse/travellist_starterproject/TravelListAdapter.java
@@ -1,6 +1,9 @@
 package bhouse.travellist_starterproject;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -29,10 +32,18 @@ public class TravelListAdapter extends RecyclerView.Adapter<TravelListAdapter.Vi
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(final ViewHolder holder, int position) {
         final Place place = PlaceData.placeList().get(position);
         holder.placeName.setText(place.name);
         Picasso.with(mContext).load(place.getImageResourceId(mContext)).into(holder.placeImage);
+        Bitmap photo = BitmapFactory.decodeResource(mContext.getResources(), place.getImageResourceId(mContext));
+        Palette.from(photo)
+                .generate(new Palette.PaletteAsyncListener() {
+                    public void onGenerated(Palette palette) {
+                        int bgColor = palette.getMutedColor(mContext.getResources().getColor(android.R.color.black));
+                        holder.placeNameHolder.setBackgroundColor(bgColor);
+                    }
+                });
     }
 
     @Override
@@ -44,11 +55,12 @@ public class TravelListAdapter extends RecyclerView.Adapter<TravelListAdapter.Vi
         this.mItemClickListener = mItemClickListener;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public LinearLayout placeHolder;
         public LinearLayout placeNameHolder;
         public TextView placeName;
         public ImageView placeImage;
+
         public ViewHolder(View itemView) {
             super(itemView);
             placeHolder = (LinearLayout) itemView.findViewById(R.id.mainHolder);
